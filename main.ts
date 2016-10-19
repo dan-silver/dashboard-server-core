e41ecfac7937fc0101292b2748c0b7028864e141
Dan Silver <dannysilver3@gmail.com>
2016-10-18 23:19:00 -0700

    1

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 import * as _db from "./db";
 import * as _users from "./users";
 import * as _dashboards from "./dashboards";
-import * as _common from "./common";
-
+export * from "./common"
 
 export let db = _db;
 export let users = _users;
 export let dashboards = _dashboards;
-export let common = _common;
