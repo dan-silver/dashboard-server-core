ee0e090aa7c6d635b2a862afd252e5906e1fbd13
Dan Silver <dannysilver3@gmail.com>
2016-10-21 20:47:21 -0700

    refactoed modules

--- main.ts
+++ main.ts
@@ -3,9 +3,7 @@ import * as _users from "./users";
 import * as _dashboards from "./dashboards";
 import * as _common from "./common"
 
-export module serverCore {
-  export let users = _users;
-  export let dashboards = _dashboards;
-  export let db = _db;
-  export let common = _common;
-}
+export let users = _users;
+export let dashboards = _dashboards;
+export let db = _db;
+export let common = _common;
