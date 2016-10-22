577598ebc04349ada34ed9e3ff61d2918e6aef62
Dan Silver <dannysilver3@gmail.com>
2016-10-21 22:00:48 -0700

    refactoed modules

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,8 +1,7 @@
 import * as _db from "./db";
 import * as _users from "./users";
 import * as _dashboards from "./dashboards";
-import * as _common from "./common";
+export { StandardCallback, ErrorCallback, User, Dashboard, SourceNames } from "./common";
 export declare let users: typeof _users;
-export declare let common: typeof _common;
 export declare let dashboards: typeof _dashboards;
 export declare let db: typeof _db;
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import * as _db from "./db";
 import * as _users from "./users";
 import * as _dashboards from "./dashboards";
-import * as _common from "./common"
+export {StandardCallback, ErrorCallback, User, Dashboard, SourceNames} from "./common"
 
 export let users = _users;
-export let common = _common;
 export let dashboards = _dashboards;
 export let db = _db;
