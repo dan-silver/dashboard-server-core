2a61402f11b4b33705f263d97856ff676d85fe05
Dan Silver <dannysilver3@gmail.com>
2016-10-21 21:54:48 -0700

    refactoed modules

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import * as _users from "./users";
 import * as _dashboards from "./dashboards";
 import * as _common from "./common"
 
-export default class foo {
-  static users = _users;
-  static dashboards = _dashboards;
-  static db = _db;
-  static common = _common;
+export module foo {
+  export let users = _users;
+}
+
+export module fooCommon {
+  export let common = _common;
 }
