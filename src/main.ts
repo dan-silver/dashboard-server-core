275e2318cccce1e489a78c3699af5d14f43f1aad
Dan Silver <dannysilver3@gmail.com>
2016-10-21 21:45:57 -0700

    refactoed modules

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as _users from "./users";
 import * as _dashboards from "./dashboards";
 import * as _common from "./common"
 
+module.exports = {}
 module.exports.users = _users;
 module.exports.dashboards = _dashboards;
 module.exports.db = _db;
