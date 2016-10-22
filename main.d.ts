82b443ee4eb045aa0d60d41357fdf5ab321386d3
Dan Silver <dannysilver3@gmail.com>
2016-10-21 21:28:14 -0700

    refactoed modules

--- main.d.ts
+++ main.d.ts
@@ -2,9 +2,7 @@ import * as _db from "./db";
 import * as _users from "./users";
 import * as _dashboards from "./dashboards";
 import * as _common from "./common";
-export declare module serverCore {
-    let users: typeof _users;
-    let dashboards: typeof _dashboards;
-    let db: typeof _db;
-    let common: typeof _common;
-}
+export declare let users: typeof _users;
+export declare let dashboards: typeof _dashboards;
+export declare let db: typeof _db;
+export declare let common: typeof _common;
