0259b66d8496e0d09d510ebc377839390a48a2b1
Dan Silver <dannysilver3@gmail.com>
2016-11-03 00:06:01 -0700

    1

--- src/users.ts
+++ src/users.ts
@@ -4,9 +4,9 @@ import * as db from "./db";
 import  * as common from "./common"
 import {ObjectID} from 'mongodb';
 
-export let findById = (userId:ObjectID, callback:common.StandardCallback<common.User>) => {
+export let findById = (userId:string, callback:common.StandardCallback<common.User>) => {
   db.get((db) => {
-    db.collection('users').find({_id: userId}).toArray((err, user) => {
+    db.collection('users').find({_id: new ObjectID(userId)}).toArray((err, user) => {
       callback(err, user[0]);
       db.close();
   });
@@ -14,7 +14,7 @@ export let findById = (userId:ObjectID, callback:common.StandardCallback<common.
 };
 
 
-export let getAuthScopes = (userId:ObjectID, cb:common.StandardCallback<string[]>) => {
+export let getAuthScopes = (userId:string, cb:common.StandardCallback<string[]>) => {
   findById(userId, (err, user) => {
       var allScopes:string[] = [];
 
@@ -49,10 +49,10 @@ export let create = (user:common.User, cb:common.StandardCallback<ObjectID>) => 
   });
 }
 
-export let deleteAccount = (userId:ObjectID, callback:common.StandardCallback<any>) => {
+export let deleteAccount = (userId:string, callback:common.StandardCallback<any>) => {
   db.get((db) => {
     db.collection('users').deleteOne({
-      _id: userId
+      _id: new ObjectID(userId)
     }, function(err) {
       callback(err);
       db.close();
@@ -61,7 +61,7 @@ export let deleteAccount = (userId:ObjectID, callback:common.StandardCallback<an
 
 }
 
-export let updateAuthScopes = (userId:ObjectID, authServiceName:string, scopes:string[], callback:common.ErrorCallback) => {
+export let updateAuthScopes = (userId:string, authServiceName:string, scopes:string[], callback:common.ErrorCallback) => {
   var authInfoPath = "auth." + authServiceName + ".scopes";
   let updateObj:{ [id: string] : any; }  = {};
   updateObj[authInfoPath] = {$each: scopes};
@@ -69,7 +69,7 @@ export let updateAuthScopes = (userId:ObjectID, authServiceName:string, scopes:s
 
   db.get((db) => {
     db.collection('users').update({
-      _id: userId
+      _id: new ObjectID(userId)
     },{
       $addToSet: updateObj,
     }, function(err) {
@@ -79,7 +79,7 @@ export let updateAuthScopes = (userId:ObjectID, authServiceName:string, scopes:s
 }
 
 
-export let updateAuthInfo = (userId:ObjectID, authServiceName:string, authInfo:any, callback:common.StandardCallback<common.User>) => {
+export let updateAuthInfo = (userId:string, authServiceName:string, authInfo:any, callback:common.StandardCallback<common.User>) => {
   var authInfoPath = "auth." + authServiceName;
   var updateObj:{ [id: string] : string; }  = {};
   updateObj[authInfoPath] = authInfo;
@@ -87,7 +87,7 @@ export let updateAuthInfo = (userId:ObjectID, authServiceName:string, authInfo:a
 
   db.get((db) => {
     db.collection('users').update({
-      _id: userId
+      _id: new ObjectID(userId)
     },{
       $set: updateObj,
     }, function(err) {
@@ -103,14 +103,14 @@ export let updateAuthInfo = (userId:ObjectID, authServiceName:string, authInfo:a
   });
 }
 
-export let removeAuthInfo = (userId:ObjectID, authServiceName:string, callback:common.StandardCallback<common.User>) => {
+export let removeAuthInfo = (userId:string, authServiceName:string, callback:common.StandardCallback<common.User>) => {
   var authInfoPath = "auth." + authServiceName;
   var updateObj:{ [id: string] : any; }  = {};
   updateObj[authInfoPath] = {};
 
   db.get((db) => {
     db.collection('users').update({
-      _id: userId
+      _id: new ObjectID(userId)
     },{
       $unset: updateObj,
     }, function(err) {
