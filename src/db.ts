37d15b531f7505d374a3584d63471cd9f65e25d7
Dan Silver <dannysilver3@gmail.com>
2016-11-02 23:32:51 -0700

    mongo url

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,14 @@
 /// <reference path="../typings/index.d.ts" />
 
-// let url = 'mongodb://localhost:4321/social-dash';
-
-
 import * as mongo from 'mongodb'
+import {MONGO_URL} from "./common"
 
 export interface DbCallback {
   (db: mongo.Db):void;
 }
 
 export function get(callback:DbCallback):void {
-  mongo.MongoClient.connect(process.env.MONGO_URL, (err, db) => {
+  mongo.MongoClient.connect(MONGO_URL, (err, db) => {
     if (err) {
       console.log(err);
       return;
