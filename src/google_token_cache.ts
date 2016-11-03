280f255c2110dbade4e56b32867f89948767d51a
Dan Silver <dannysilver3@gmail.com>
2016-11-02 23:17:12 -0700

    added google token cache

---
+++ src/google_token_cache.ts
@@ -0,0 +1,98 @@
+/// <reference path="../typings/index.d.ts" />
+
+var ObjectId = require('mongodb').ObjectID;
+var request = require('superagent');
+
+import  * as common from "./common"
+import * as users from "./users"
+
+
+/*
+ {
+    userId: {
+      authServiceName: {
+        accessToken: 
+        expiresAt: Absolute date 
+      }
+    }
+*/
+
+interface TokenCache {
+  [userId:string]: {
+      [serviceName:string]: {
+          accessToken: string,
+          expiresAt: Date
+      }
+  }
+}
+
+let cache:TokenCache = {};
+
+let GOOGLE_CLIENT_ID:string = null;
+let GOOGLE_CLIENT_SECRET:string = null;
+
+export function init(_GOOGLE_CLIENT_ID: string, _GOOGLE_CLIENT_SECRET:string) {
+  GOOGLE_CLIENT_ID = _GOOGLE_CLIENT_ID;
+  GOOGLE_CLIENT_SECRET = _GOOGLE_CLIENT_SECRET;
+}
+
+
+export function getGoogleAccessToken(userId:string, callback:common.StandardCallback<string>) {
+  if (userId in cache && "google" in cache[userId]) {
+    let authInfo = cache[userId]["google"]
+    if (authInfo.expiresAt > new Date((new Date()).getTime() + 5*60000)) { //expires at least 5 minutes from now
+      callback(null, authInfo.accessToken);
+      return;
+    }
+  }
+
+
+  getGoogleAccessTokenFromRefreshToken(userId, callback);
+
+}
+
+
+export function getGoogleAccessTokenFromRefreshToken(userId:string, callback:common.StandardCallback<string>) {
+  if (GOOGLE_CLIENT_ID == null) {
+    callback({err: 'GOOGLE_CLIENT_ID not set'});
+    return;
+  }
+
+  users.findById(userId, (err, user)=> {
+
+    const userGoogleAuthInfo = user.auth["google"];
+    let refreshToken = userGoogleAuthInfo.refreshToken;
+
+    request
+      .post('https://www.googleapis.com/oauth2/v4/token')
+      .type('form')
+      .send({
+        client_id: GOOGLE_CLIENT_ID,
+        grant_type: "refresh_token",
+        client_secret: GOOGLE_CLIENT_SECRET,
+        refresh_token: refreshToken
+      })
+      .end(function(err:any, res:any) {
+        const access_token = res.body.access_token;
+
+        // save to cache
+        setAccessToken(userId, "google", access_token, new Date((new Date()).getTime() + res.body.expires_in * 1000));
+
+        if (err) {
+          callback(err)
+        } else {
+          callback(null, res.body.access_token);
+        }
+        // Calling the end function will send the request
+      });
+  })
+
+}
+
+export function setAccessToken(userId:string, authServiceName:string, accessToken:string, expiresAt:Date) {
+  cache[userId] = cache[userId] || {};
+  cache[userId][authServiceName] = {
+    accessToken: accessToken,
+    expiresAt: expiresAt ? expiresAt : new Date((new Date()).getTime() + 1800 * 1000) //if no expiresAt, set +30min
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import * as _db from "./db";
 import * as _users from "./users";
 import * as _dashboards from "./dashboards";
+import * as _google_token_cache from "./google_token_cache";
+
 export {StandardCallback, ErrorCallback, User, Dashboard, SourceNames} from "./common"
 
 export let users = _users;
 export let dashboards = _dashboards;
 export let db = _db;
+export let google_token_cache = _google_token_cache;
